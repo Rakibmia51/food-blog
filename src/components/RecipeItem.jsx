3715f1133786c047efae921ffb45e38812785a67
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-12-09 23:45:02 +0600

    Food Blog App

---
+++ src/components/RecipeItem.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
+import foodImg from '../assets/foodRecipe.png'
+import { BsFillStopwatchFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import axios from 'axios';
+
+export default function RecipeItem() {
+    const recipes=useLoaderData()
+    const [getAllRecipes, setAllRecipes]=useState()
+    let path = window.location.pathname==="/myRecipe" ? true : false
+    let favItems=JSON.parse(localStorage.getItem("fav")) ?? []
+    const [isFavRecipe, setIsFavRecipe]=useState(false)
+    // console.log(getAllRecipes)
+
+    useEffect(()=>{
+        setAllRecipes(recipes)
+    },[recipes])
+
+    const onDelete= async(id)=>{
+        await axios.delete(`https://full-stack-project-axar.onrender.com/recipe/${id}`)
+            .then((res)=>console.log(res))
+            setAllRecipes(recipes=>recipes.filter(recipe=>recipe._id !==id))
+
+            let filterItem = favItems.filter(recipe=>recipe._id !== id)
+            localStorage.setItem("fav",JSON.stringify(filterItem))
+    }
+
+    const favRecipe = (item)=>{
+        let filterItem = favItems.filter(recipe=>recipe._id !==item._id)
+        favItems = favItems.filter(recipe=>recipe._id === item._id).length===0 ? [...favItems,item]: filterItem
+        localStorage.setItem("fav",JSON.stringify(favItems))
+        setIsFavRecipe(pre=>!pre)
+    }
+  return (
+    <>
+        <div className='card-container'>
+            {
+                getAllRecipes?.map((item, index)=>{
+                    return (
+                        <div key={index} className='card'>
+                            <img src={`https://full-stack-project-axar.onrender.com/images/${item.coverImage}`} width="120px" height="100px"></img>
+                            <div className='card-body'>
+                                <div className='title'>{item.title}</div>
+                                <div className='icons'>
+                                    <div className='timer'><BsFillStopwatchFill/>{item.time}</div>
+                                    {   
+                                        (!path) ?<FaHeart onClick={()=>favRecipe(item)}
+                                        style={{color: (favItems.some(res => res._id === item._id)) ? "red" : ""}}/>:
+                                        <div className='action'>
+                                           <Link to={`/editRecipe/${item._id}`} className='editIcon'><FaEdit/></Link>
+                                            <MdDelete onClick={()=>onDelete(item._id)} className='deleteIcon'/>
+                                        </div>
+                                    
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </>
+  )
+}
